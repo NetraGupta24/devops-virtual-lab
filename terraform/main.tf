3bd9bc9f3954658943ff9ee5196ca3c15a9a88d7
Netra Gupta <24122004.ng@gmail.com>
2026-09-20 20:49:22 +0000

    Initial DevOps VLE implementation

---
+++ terraform/main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Find the latest Ubuntu 24.04 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Security Group
+resource "aws_security_group" "devops_sg" {
+
+  name = "devops-lab-security-group"
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Application port
+  ingress {
+    description = "Application"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubernetes NodePort
+  ingress {
+    description = "Kubernetes NodePort"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "devops_server" {
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.small"
+
+  key_name = "devops-lab-key"
+
+  vpc_security_group_ids = [
+    aws_security_group.devops_sg.id
+  ]
+
+  tags = {
+    Name = "DevOps-Lab-Server"
+  }
+}
+
+# Outputs
+output "public_ip" {
+  value = aws_instance.devops_server.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.devops_server.public_dns
+}
